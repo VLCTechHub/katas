d18a773748604821134267f9377c3a511f794ba8
Jesús Plou <jesusplou@tirant.com>
2024-03-01 14:42:51 +0100

    Apply code formatting

--- RomanNumbers/typeScript/carlos-jesus/src/conversor.ts
+++ RomanNumbers/typeScript/carlos-jesus/src/conversor.ts
@@ -1,18 +1,18 @@
 export function conversor(input: number) {
-    let romanNumber = ''
+  let romanNumber = "";
 
-    if (input >= 5) {
-        romanNumber += 'V'
-        input -= 5
-    }
-
-    for (let i=0; i < input; i++) {
-        romanNumber += 'I'
-    }
-    
-    if (romanNumber == 'IIII') {
-        romanNumber = 'IV'
-    }
-    
-    return romanNumber;
+  if (input >= 5) {
+    romanNumber += "V";
+    input -= 5;
   }
+
+  for (let i = 0; i < input; i++) {
+    romanNumber += "I";
+  }
+
+  if (romanNumber == "IIII") {
+    romanNumber = "IV";
+  }
+
+  return romanNumber;
+}
--- RomanNumbers/typeScript/carlos-jesus/src/index.test.ts
+++ RomanNumbers/typeScript/carlos-jesus/src/index.test.ts
@@ -1,37 +1,37 @@
-import { conversor } from './conversor'
+import { conversor } from "./conversor";
 
-describe('Index', () => {
-  it('converts 1 to I', () => {
-    assertRomanNumber(1, 'I')
+describe("Index", () => {
+  it("converts 1 to I", () => {
+    assertRomanNumber(1, "I");
   });
 
-  it('converts 2 to II', () => {
-    assertRomanNumber(2, 'II')
+  it("converts 2 to II", () => {
+    assertRomanNumber(2, "II");
   });
 
-  it('converts 3 to III', () => {
-    assertRomanNumber(3, 'III')
+  it("converts 3 to III", () => {
+    assertRomanNumber(3, "III");
   });
 
-  it('converts 4 to IV', () => {
-    assertRomanNumber(4,'IV')
-  })
-  
-  it('converts 5 to V', () => {
-    assertRomanNumber(5,'V')
-  })
+  it("converts 4 to IV", () => {
+    assertRomanNumber(4, "IV");
+  });
 
-  it('converts 6 to VI', () => {
-    assertRomanNumber(6,'VI')
-  })
+  it("converts 5 to V", () => {
+    assertRomanNumber(5, "V");
+  });
 
-  it('converts 8 to VIII', () => {
-    assertRomanNumber(8,'VIII')
-  })
+  it("converts 6 to VI", () => {
+    assertRomanNumber(6, "VI");
+  });
+
+  it("converts 8 to VIII", () => {
+    assertRomanNumber(8, "VIII");
+  });
 
   function assertRomanNumber(input: number, expectedRomanNumber: string) {
-    const result = conversor(input)
+    const result = conversor(input);
 
-    expect(result).toEqual(expectedRomanNumber)
+    expect(result).toEqual(expectedRomanNumber);
   }
 });
